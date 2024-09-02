b0e110d0bdcbdf1143e0e0cf7cea409b1b80be67
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2024-09-03 00:08:39 +0800

    chore: Add initial data and dependencies

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,48 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
 const Form = () => {
-	return (
-		<div className="add-form">
-			<h3>What do you need for your trip?</h3>
-		</div>
-	);
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validate description
+    if (!description) return;
+
+    // Validate quantity
+    if (quantity <= 0) return;
+
+    const newItem = { description, quantity, packed: false, id: uuidv4() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  };
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
 };
 
 export default Form;
--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,5 +1,16 @@
+import { initialItems } from "../../data";
+import Item from "./Item";
+
 const PackingList = () => {
-	return <div className="list">PackingList</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default PackingList;
